90303064fca027968f78b148a88bf3ce5bdf86b1
jbcayan <jbc.ayan17@gmail.com>
2024-10-27 22:51:01 +0600

    Add echo statements to render_build.sh for process feedback

--- render_build.sh
+++ render_build.sh
@@ -2,12 +2,16 @@ echo "render_build.sh running"
 
 # exit on error
 set -o errexit
+echo "no errors"
 
 # install python dependencies
-pip install -r requirements.txt
+pip install -r ../requirements.txt
+echo "python dependencies installed"
 
 # collect static files
 python throwin/manage.py collectstatic --noinput
+echo "static files collected"
 
 # migrate
 python throwin/manage.py migrate
+echo "db migrated"
