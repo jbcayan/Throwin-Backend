6b7657b1ccebcdd8600296393b1b900fa2cc741a
jbcayan <jbc.ayan17@gmail.com>
2024-10-27 15:02:06 +0600

    Add comments to build script for clarity on each step

--- throwin/build.sh
+++ throwin/build.sh
@@ -1,7 +1,11 @@
+# exit on error
 set -o errexit
 
+# install python dependencies
 pip install -r requirements.txt
 
+# collect static files
 python manage.py collectstatic --noinput
 
+# migrate
 python manage.py migrate
