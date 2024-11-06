114eddbe47704ea006938c441135e780b19ec28a
jbcayan <jbc.ayan17@gmail.com>
2024-11-06 18:09:51 +0600

    added Store List, Create, Detail, Update, Destroy API

--- throwin/store/rest/urls/__init__.py
+++ throwin/store/rest/urls/__init__.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
 urlpatterns = [
+    path("", include("store.rest.urls.store")),
     path("/<str:store_code>/stuff", include("store.rest.urls.store_stuff")),
 ]
---
+++ throwin/store/rest/views/store.py
@@ -0,0 +1,66 @@
+"""Views for store"""
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from store.models import Store
+from store.rest.serializers.store import StoreSerializer
+
+from common.permissions import (
+    IsConsumerOrGuestUser,
+    IsConsumerUser,
+    IsAdminUser,
+    IsSuperAdminUser,
+    CheckAnyPermission
+)
+
+
+class StoreListCreate(generics.ListCreateAPIView):
+    serializer_class = StoreSerializer
+
+    available_permission_classes = (
+        IsSuperAdminUser
+    )
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            self.available_permission_classes = (
+                IsAdminUser,
+            )
+        else:
+            self.available_permission_classes = (
+                IsConsumerOrGuestUser,
+                IsConsumerUser,
+                IsAdminUser,
+            )
+        return (CheckAnyPermission(),)
+
+    def get_queryset(self):
+        queryset = Store().get_all_actives()
+        return queryset
+
+
+class StoreDetailUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StoreSerializer
+
+    available_permission_classes = (
+        IsSuperAdminUser
+    )
+
+    def get_permissions(self):
+        if self.request.method == "PUT":
+            self.available_permission_classes = (
+                IsAdminUser,
+            )
+        else:
+            self.available_permission_classes = (
+                IsConsumerOrGuestUser,
+                IsConsumerUser,
+                IsAdminUser,
+            )
+        return (CheckAnyPermission(),)
+
+    def get_object(self):
+        queryset = Store().get_all_actives().get(
+            code=self.kwargs["code"]
+        )
+        return queryset
