b585433d3528408df3a2c6322ae7434acd2c7f89
jbcayan <jbc.ayan17@gmail.com>
2024-12-15 09:56:39 +0600

    Set stricter cookie policies for CSRF and session handling
    Updated CSRF and session cookies to use "Lax" for SameSite settings to enhance security. Enabled HttpOnly for session cookies while allowing JS access to CSRF cookies when needed. These changes aim to balance security and functionality in development environments.

--- throwin/throwin/settings/production.py
+++ throwin/throwin/settings/production.py
@@ -227,12 +227,18 @@ CSRF_TRUSTED_ORIGINS = [
     "http://throwin-frontend-react.s3-website-ap-northeast-1.amazonaws.com",
 ]
 
-
+# SESSION_COOKIE_SAMESITE = None
 SESSION_COOKIE_SECURE = False  # For development only; set True in production with HTTPS
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_USE_SESSIONS = True
 CSRF_COOKIE_DOMAIN = 'localhost:5173'
+
+CSRF_COOKIE_SAMESITE = "Lax"  # Ensures CSRF cookies are accessible for same-site requests
+SESSION_COOKIE_SAMESITE = "Lax"
+
+CSRF_COOKIE_HTTPONLY = False  # Allow JS to read CSRF cookies, if needed
+SESSION_COOKIE_HTTPONLY = True  # Keep session cookie secure
 # CSRF_COOKIE_DOMAIN = 'core-sm.online'
 
 REST_FRAMEWORK = {
