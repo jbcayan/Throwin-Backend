58cc6f71c142d02c0964662c4c7718f7f8b1ed11
jbcayan <jbc.ayan17@gmail.com>
2024-12-18 15:43:48 +0600

    Add detailed notification views, serializers, and routes
    Refactored notification views to support detailed administration features and dynamic serializer selection. Introduced `NotificationDetail` class for CRUD operations and added routes for listing and managing notifications. Updated serializers to include `created_by` and `updated_by` fields for admin-specific operations.

---
+++ throwin/notification/rest/urls/notification.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from notification.rest.views.notifications import (
+    NotificationList,
+    NotificationDetail
+)
+
+urlpatterns = [
+    path(
+        "",
+        NotificationList.as_view(),
+        name="notification-list"
+    ),
+    path(
+        "/<str:uid>",
+        NotificationDetail.as_view(),
+        name="notification-detail")
+]
