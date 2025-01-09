59b6b34d77ea4db30e6fa909d885e081fca6ed3a
jbcayan <jbc.ayan17@gmail.com>
2025-01-09 16:16:15 +0600

    deleted extra API for restaurant owner login

--- throwin/accounts/rest/urls/restaurant_owner.py
+++ throwin/accounts/rest/urls/restaurant_owner.py
@@ -1,12 +1,5 @@
 """URLS for restaurant owner related API views."""
-from accounts.rest.views.restaurant_owner_login import RestaurantOwnerLogin
-
 from django.urls import path
 
 urlpatterns = [
-    path(
-        "/login",
-        RestaurantOwnerLogin.as_view(),
-        name="restaurant-owner-login"
-    ),
 ]
