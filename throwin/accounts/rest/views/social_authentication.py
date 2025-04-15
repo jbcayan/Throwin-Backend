0b714b1fcc35bf2d981be5d54bed5cc18ceb68c1
jbcayan <jbc.ayan17@gmail.com>
2025-04-15 12:47:52 +0600

    fix(auth): remove unnecessary validation exception raising in social authentication

--- throwin/accounts/rest/views/social_authentication.py
+++ throwin/accounts/rest/views/social_authentication.py
@@ -28,5 +28,4 @@ class LineSignIn(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
