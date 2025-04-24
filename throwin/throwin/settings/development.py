898322bf45ac40b10773182ab3f34763d33691d5
jbcayan <jbc.ayan17@gmail.com>
2025-04-24 10:06:06 +0600

    fix(development): change default DB_HOST from localhost to 127.0.0.1

--- throwin/throwin/settings/development.py
+++ throwin/throwin/settings/development.py
@@ -142,7 +142,7 @@ else:
             "NAME": config("DB_NAME", default="throwin"),
             "USER": config("DB_USER", default="postgres"),
             "PASSWORD": config("DB_PASSWORD", default="postgres"),
-            "HOST": config("DB_HOST", default="localhost"),
+            "HOST": config("DB_HOST", default="127.0.0.1"),
             "PORT": config("DB_PORT", default="5432"),
             "OPTIONS": {
                 "sslmode": "require",  # Add this line if SSL is required
