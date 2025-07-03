15af6a0d114cb06cb26e4a04040c133bc47c5c3a
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2025-07-03 12:41:01 +0600

    feat(entrypoint): add conditional migration logic for SQLite database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Navigate to the project directory
 cd /app/throwin
 
-# Wait for Redis to be ready
 echo "Waiting for Redis..."
 while ! nc -z redis_cache 6379; do
   sleep 1
 done
 echo "Redis is up!"
 
-# Apply database migrations
-echo "Applying migrations..."
-python manage.py migrate --noinput
+# Conditional migration logic
+if [ "$DATABASE_TYPE" = "sqlite" ]; then
+  if [ -f "/app/throwin/db.sqlite3" ]; then
+    echo "SQLite DB already exists, skipping migrations..."
+  else
+    echo "No SQLite DB found, applying migrations..."
+    python manage.py migrate --noinput
+  fi
+else
+  echo "Applying migrations for non-SQLite DB..."
+  python manage.py migrate --noinput
+fi
 
-# Ensure staticfiles directory exists
-#echo "Ensuring staticfiles directory exists..."
-#mkdir -p /app/staticfiles
-
-# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear -v 2 || echo "Static files collection failed but continuing."
 
-# Start Celery worker in the background
 echo "Starting Celery worker..."
 celery -A throwin worker --loglevel=info &
 
-# Start Celery beat in the background
 echo "Starting Celery beat..."
 celery -A throwin beat --loglevel=info &
 
-echo "Starting Django development server..."
-#python manage.py runserver 0.0.0.0:8000
-
-# Start the Gunicorn server
+echo "Starting Gunicorn server..."
 exec gunicorn throwin.wsgi:application --bind 0.0.0.0:8000
