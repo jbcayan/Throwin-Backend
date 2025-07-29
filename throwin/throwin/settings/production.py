0e8dbc731dd718482679edbc27fd3947008f47ff
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2025-07-29 13:27:59 +0600

    feat(development, production): comment out SessionAuthentication in REST framework settings

--- throwin/throwin/settings/production.py
+++ throwin/throwin/settings/production.py
@@ -276,7 +276,7 @@ SESSION_COOKIE_SECURE = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
